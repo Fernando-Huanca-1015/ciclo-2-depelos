0e02b8fe913d7f43955bc00df11cb074b0de45fa
dmamanipar <dmamanipar@unsa.edu.pe>
2024-10-04 11:19:58 -0500

    CRUD cmd con Spring y Acceso a Base de datos

--- SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/SysAlmacenFxApplication.java
+++ SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/SysAlmacenFxApplication.java
@@ -1,13 +1,30 @@
 package pe.edu.upeu.sysalmacenfx;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import pe.edu.upeu.sysalmacenfx.repositorio.CategoriaRepository;
+import pe.edu.upeu.sysalmacenfx.repositorio.MainX;
 
 @SpringBootApplication
 public class SysAlmacenFxApplication {
+	@Autowired
+	static CategoriaRepository repository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SysAlmacenFxApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner run(ApplicationContext context) {return  args ->{
+		MainX mx =context.getBean(MainX.class);
+		mx.menu();
+	};
+	}
 }
